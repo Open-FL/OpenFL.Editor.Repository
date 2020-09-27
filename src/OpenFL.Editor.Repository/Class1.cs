85a00c18485f3cefeb0292eedd645cffb829cd69
Tim Akermann <ti.akermann@gmail.com>
2020-09-27 20:06:50 +0200

    Initial Commit.

---
+++ src/OpenFL.Editor.Repository/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenFL.Editor.Repository
+{
+    public class Class1
+    {
+    }
+}
